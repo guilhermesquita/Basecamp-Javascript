9fa255a733a5f3eecb25006fb236c10e0fbc7fea
Guilherme Mesquita <52768525+guilhermesquita@users.noreply.github.com>
2022-04-26 11:53:54 -0400

    Add files via upload

---
+++ js_dom/scripts.js
@@ -0,0 +1,8 @@
+let corpo = document.getElementsByTagName('body')[0]
+let a = document.getElementsByClassName("btn")[0]
+a.addEventListener('click', clicar)
+
+function clicar(){      
+        corpo.style.background = 'black'
+}
+    
