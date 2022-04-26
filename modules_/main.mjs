2a75b3d8885d1a8570ffd64434aea6ecb90df782
Guilherme Mesquita <52768525+guilhermesquita@users.noreply.github.com>
2022-04-26 10:38:10 -0400

    Add files via upload

---
+++ modules_/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Guilherme", 18));
+console.log(mostraCidade("Guilherme", 'Manaus'));
+console.log(mostraHobby("Guilherme", 'Compor Músicas   '));
